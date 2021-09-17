2be5f501d60a0f3a53721582042b420a5606033d
Anton <antopavlu@gmail.com>
2021-09-17 23:03:51 +0300

    loooseesefsd

--- src/components/UserImg/UserImg.js
+++ src/components/UserImg/UserImg.js
@@ -6,7 +6,7 @@ class UserImg extends React.Component {
     super(props);
 
     this.state = {
-      hide: true,
+      hide: false,
     };
   }
 
@@ -16,25 +16,18 @@ class UserImg extends React.Component {
     });
   };
 
-  loadHandler = () => {
-    this.setState({
-      hide : false
-    })
-  }
-
   render() {
     const { className, src, alt } = this.props;
-    const { hide } = this.state;
 
-    const img = <img onLoad={this.loadHandler} onError={this.errorHandler} className={className} src={src} alt={alt}
+    const img = <img onError={this.errorHandler} className={className} src={src} alt={alt}
       />
     const par = <p className="initials">{alt}</p>
- 
-    const elem = hide ? img : par;
+    
 
     return (
       <div className="imgWrapper">
-         {elem}
+         {img}
+         {par}
       </div>
     );
   }

c8cf47ee09cf6dc905b57b91dbb8d3b667840153
Anton <antopavlu@gmail.com>
2021-09-17 22:40:01 +0300

    add state

--- src/components/UserImg/UserImg.js
+++ src/components/UserImg/UserImg.js
@@ -6,17 +6,32 @@ class UserImg extends React.Component {
     super(props);
 
     this.state = {
-      hide: "hideBlock",
+      hide: true,
     };
   }
 
+  errorHandler = () => {
+    const { hide } = this.state;
+
+    this.setState({
+      hide: !hide,
+    });
+  };
+
   render() {
     const { className, src, alt } = this.props;
+    const { hide } = this.state;
 
+    const img = <img onError={this.errorHandler} className={className} src={src} alt={alt}
+      />
+    const par = <p className="initials">{alt}</p>
+   
+    
+ 
     return (
       <div className="imgWrapper">
-        <img className={className} src={src} alt={alt} />
-        <p className="initials">{alt}</p>
+         {img}
+         {par} 
       </div>
     );
   }

e11380cffaaa1054843367b0ca7364d8ee07ddf8
Anton <antopavlu@gmail.com>
2021-10-18 19:30:19 +0300

    add some really cole styles

--- src/components/ToDoList/ToDoInput/ToDoInput.jsx
+++ src/components/ToDoList/ToDoInput/ToDoInput.jsx
@@ -2,26 +2,33 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 import { TASK_BODY_SCHEMA } from "../../../utils/schema";
 
-import styles from './ToDoInput.module.scss'
+import styles from "./ToDoInput.module.scss";
 const Todoinput = (props) => {
+  const { addTask } = props;
 
-  const {addTask} = props;
-
-  const handleSubmit = (values,formikBag) => {
-    addTask(values.task) 
-    formikBag.resetForm()
-  }
+  const handleSubmit = (values, formikBag) => {
+    addTask(values.task);
+    formikBag.resetForm();
+  };
 
   return (
-    <div className ={styles.formWrapper}>
-    <h1 className={styles.heading}>Your personal todos</h1>
-    <Formik onSubmit={handleSubmit} validationSchema={TASK_BODY_SCHEMA} initialValues={{ task: "" }}>
-      <Form className={styles.form}>
-        <Field className={styles.input} name="task" />
-        <ErrorMessage name="task"></ErrorMessage>
-        <button className={styles.btn} type='submit'><i className='fa fa-plus'></i></button>
-      </Form> 
-    </Formik>
+    <div className={styles.formWrapper}>
+      <h1 className={styles.heading}>Your personal todos</h1>
+      <Formik
+        onSubmit={handleSubmit}
+        validationSchema={TASK_BODY_SCHEMA}
+        initialValues={{ task: "" }}
+      >
+        <Form className={styles.form}>
+          <Field className={styles.input} name="task" />
+          <ErrorMessage name="task">
+            {(msg) => <div className={styles.errorMessage}>{msg}</div>}
+          </ErrorMessage>
+          <button className={styles.btn} type="submit">
+            <i className="fa fa-plus"></i>
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
